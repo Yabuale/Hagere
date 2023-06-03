191bf5bac8a3287f9f63a9e9609178561386c99f
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2023-06-03 11:04:33 +0300

    delete button

--- delete.php
+++ delete.php
@@ -4,7 +4,7 @@ $EMAIL = $_GET["email"];
 $sql = "DELETE FROM customers WHERE EMAIL = '$EMAIL' ";
 
 if (mysqli_query($conn, $sql)) {
-    echo "Record deleted successfully.";
+    echo "<script>alert('Deleted successfully.'); window.location.href = 'delUpcustomer.php';</script>";
 } else {
     echo "Error deleting record: " . mysqli_error($conn);
 }
