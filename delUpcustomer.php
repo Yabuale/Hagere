3ec8f4dd439bbc92bc13e35fb85c755ddec609a3
Yeabsra Alebachew <yeabsraalebachew9@gmail.com>
2023-06-03 10:23:28 +0300

    added styles to update ad adremve.php

--- delUpcustomer.php
+++ delUpcustomer.php
@@ -1,34 +1,83 @@
 <?php
 include 'connection.php';
-
-$sqlretrive = "SELECT FNAME, LNAME, LOCATIONS, EMAIL FROM customers";
-$result = $conn->query($sqlretrive);
-
-if ($result->num_rows > 0) {
-   echo "<table>";
-  echo "<tr><th style='border: 1px solid blue;'>first name</th>
-  <th style='border: 1px solid black;'>last anme</th>
-  <th style='border: 1px solid black;'>location</th>
-  <th style='border: 1px solid black;'>email</th>
-  <th style='border: 1px solid black;'>action</th>
-  </tr>"; 
-  while($row = $result->fetch_assoc()) {
-    echo "<tr>
-    <td style='border: 1px solid black;'>".$row["FNAME"]."</td>
-    <td style='border: 1px solid black;'>".$row["LNAME"]."</td>
-    <td style='border: 1px solid black;'>".$row["LOCATIONS"]."</td>
-    <td style='border: 1px solid black;'>".$row["EMAIL"]."</td>
-    <td><a href='delete.php?email=".$row["EMAIL"]."'> delete</a>
-    <a href='update.php?email=".$row["EMAIL"]."'> update</a></td>
-</tr>";
-}
-echo "</table>";
-    
-  }
-   else {
-    echo "0 results";
-  }
-
-
-$conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        table {
+            margin: 0 auto;
+            border-collapse: collapse;
+            width: 80%;
+            font-family: Arial, sans-serif;
+        }
+
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        td.actions {
+            white-space: nowrap;
+        }
+
+        td.actions a {
+            color: #fff;
+            padding: 8px 12px;
+            border-radius: 4px;
+            text-decoration: none;
+            margin-right: 5px;
+            font-size: 14px;
+        }
+
+        td.actions .delete-btn {
+            background-color: #ff4646;
+        }
+
+        td.actions .update-btn {
+            background-color: #4caf50;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    $sqlretrive = "SELECT FNAME, LNAME, LOCATIONS, EMAIL FROM customers";
+    $result = $conn->query($sqlretrive);
+
+    if ($result->num_rows > 0) {
+        echo "<table>";
+        echo "<tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Location</th>
+            <th>Email</th>
+            <th>Action</th>
+        </tr>";
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
+                <td>".$row["FNAME"]."</td>
+                <td>".$row["LNAME"]."</td>
+                <td>".$row["LOCATIONS"]."</td>
+                <td>".$row["EMAIL"]."</td>
+                <td class='actions'>
+                    <a href='delete.php?email=".$row["EMAIL"]."' class='delete-btn'>Delete</a>
+                    <a href='update.php?email=".$row["EMAIL"]."' class='update-btn'>Update</a>
+                </td>
+            </tr>";
+        }
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+    ?>
+</body>
+</html>
